84089e96c5d8e98a4bdbc5be5fbf8e85ff791b52
Sumeng Wang <sumeng.wang.2019@anderson.ucla.edu>
2019-02-20 15:26:31 -0800

    finish project 5

--- project5/LSMC.R
+++ project5/LSMC.R
@@ -80,15 +80,93 @@ LSMC = function (N, t, K, S0, r, sigma, num, k, FUN){
   return(mean(CF%*% D))
 }
 
+LSMC_eu = function (N, t, Kt, S0, r, sigma, num){
+  paths = matrix(rep(0, len = num *(N+1)), nrow = num)
+  rand = matrix(rnorm(num*N), nrow = num)
+  dt = t/N
+  paths[, 1] = S0
+  zeros = rep(0 , len = num)
+  zeros_row = rep(0, len = N)
+  for (i in 1:N){
+    paths[, i+1] = paths[, i] *exp((r-sigma^2/2)*dt + sigma*rand[,i]*sqrt(dt))
+  }
+  strike_date = Kt/dt + 1
+  K = mean(paths[, strike_date])
+  payoff = apply(cbind(K - paths[, N+1], zeros), 1, max)
+  return(mean(exp(-r*t)*payoff))
+}
+
+LSMC_us = function (N, t, Kt, S0, r, sigma, num, k, FUN){
+  paths = matrix(rep(0, len = num *(N+1)), nrow = num)
+  rand = matrix(rnorm(num*N), nrow = num)
+  CF = matrix(rep(0, len = num *(N)), nrow = num)
+  dt = t/N
+  paths[, 1] = S0
+  zeros = rep(0 , len = num)
+  zeros_row = rep(0, len = N)
+  for (i in 1:N){
+    paths[, i+1] = paths[, i] *exp((r-sigma^2/2)*dt + sigma*rand[,i]*sqrt(dt))
+  }
+  strike_date = Kt/dt + 1
+  K = mean(paths[, strike_date])
+  CF[, N] = apply(cbind(K - paths[,N+1], zeros), 1, max)
+  for (i in (N-1) : strike_date){
+    temp = (K - paths[,i+1] > 0)
+    x = paths[, i+1] * temp
+    dis_factor = exp(seq(-r*dt,-(N-i)*r*dt, -r*dt))
+    if (i == N-1){
+      y = CF[, (i+1):N] * dis_factor * temp
+    }else{
+      y = CF[, (i+1):N] %*% dis_factor * temp
+    }
+    pos = seq(1, num, 1) * temp
+    y_clean = y[pos]
+    x_clean = x[pos]
+    A = matrix(rep(0, len = k*k), nrow = k)
+    B = matrix(rep(0, len =k), nrow = k)
+    X = c()
+    for (a in 1:k){
+      for (b in 1:k){
+        A[a,b] = sum(FUN(x_clean, a) * FUN(x_clean, b))
+      }
+      B[a,1] = sum(FUN(x_clean,a) * y_clean)
+      X = cbind(X, FUN(x, a))
+    }
+    reg = solve(A,B)
+    continuation = X %*% reg * temp
+    newCF = apply(cbind(K - paths[,i+1], zeros), 1, max)
+    
+    for (j in 1: num){
+      if (newCF[j] > continuation[j]){
+        CF[j, ] = zeros_row
+        CF[j,i] = newCF[j]
+      }
+    }
+  }
+  D = exp(seq(-r*strike_date*dt,-(N)*r*dt, -r*dt))
+  return(mean(CF[,strike_date:N]%*% D))
+}
 
 N = 250
-S0 = 36
+S0 = 40
 r = 0.06
 sigma = 0.2
 K = 40
+t = 0.5
+num = 100000
+k = 3
+out = LSMC(N, t, K, S0, r, sigma, num, k, Monomial)
+#out2 = LSMC(N, t, K, S0, r, sigma, num, k, Hermit)
+#out3 = LSMC(N, t, K, S0, r, sigma, num, k, Lagur)
+
+N = 200
+S0 = 65
+r = 0.06
+sigma = 0.2
+Kt = 0.2
 t = 1
 num = 100000
 k = 3
-#out = LSMC(N, t, K, S0, r, sigma, num, k, Monomial)
-#out2 = LSMC(N, t, K, S0, r, sigma, num, k, Hermit)
-out3 = LSMC(N, t, K, S0, r, sigma, num, k, Lagur)
+out4 = LSMC_eu(N, t, Kt, S0, r, sigma, num)
+
+out5 = LSMC_us(N, t, Kt, S0, r, sigma, num, k, Monomial)
